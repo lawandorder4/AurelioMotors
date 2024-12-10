bf84fd58fa19b6977b0d4ef2d9b02a98ef253ab2
santiago cardenas <scardenas@brooklynsteamcenter.org>
2024-12-10 10:50:28 -0500

    upgrade

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
 from flask import Flask, render_template
+import pymysql
+from dynaconf import Dynaconf
 
 app = Flask(__name__)
 
+conf = Dynaconf(
+    settings_files = ["settings.toml"]
+)
+
+
+
+def connect_db():
+    conn = pymysql.connect(
+        host = "10.100.34.80",
+        database = "scardenas_aureliomotors",
+        user = 'scardenas',
+        password = conf.password,
+        autocommit = True,
+        cursorclass = pymysql.cursors.DictCursor
+
+    )
+
+    return conn
+   
 @app.route("/")
 def index():
     return render_template("homepage.html.jinja")
+
+
+@app.route("/browse")
+def product_browse():
+    conn = connect_db()
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM `Product` ;")
+
+    results = cursor.fetchall()
+
+
+    return  render_template("browse.html.jinja", product = results)
+      
+    
