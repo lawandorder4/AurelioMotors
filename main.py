a6402a7db07dba09bdfc0bf50980aedca99b81ed
santiago cardenas <scardenas@brooklynsteamcenter.org>
2024-12-19 10:50:46 -0500

    progress

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-import pymysql
+from flask import Flask, render_template, request, redirect, flash, abort
 from dynaconf import Dynaconf
 
 app = Flask(__name__)
@@ -39,6 +38,92 @@ def product_browse():
     results = cursor.fetchall()
 
 
+
+    cursor.close()
+    conn.close()
+
+
     return  render_template("browse.html.jinja", product = results)
-      
+
+
+
+@app.route("/product/<product_id>")
+def product_page(product_id):
+    conn = connect_db()
+
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT * FROM `Product`WHERE `id` = {product_id};")
+
+
+
+    result = cursor.fetchone()
+    if result is None:
+          abort(404)
+
+    cursor.close()
+    conn.close()
+
+
+
+
+
+
+
+
+    return result
+
+
+@app.route("/sign_up", methods=["POST","GET"])
+def signup():
+ if flask_login.current_user.is_authenticated:
+        return redirct ("/")
+ else:
+
+
+    if request.method == "POST":
+           
+     first_name = request.form["first_name"]
+     last_name = request.form["last_name"]
     
+
+     Email_address = request.form["Email_address"]
+     Password = request.form["Password"]
+     
+
+     conn = connect_db()
+
+     cursor = conn.cursor()
+     cursor.execute(f"""
+     return redirect("/sgin_in")
+
+
+
+
+          INSTERT INTO 'Customer'
+              ('first_name', 'last_name', 'username','email','password')
+              VALUES
+              ('{first_name}', '{last_name}', '{Email_address}','{Password}')
+     """)
+     except pymysql.err.IntegrityError:
+         flash("sorry this email has already taken please try another one")
+    
+    return render_template("sign_up.html.jinja")
+
+
+
+
+
+
+      @app.route('/cart')
+      def cart 
+      return "cart page "
+
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+return render_template('404.html.jinja'), 404
+
+   
+ 
