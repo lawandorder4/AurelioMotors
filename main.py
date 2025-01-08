a1ed18ec21cc9a4607f0d22b51b0d0fb42c805d5
santiago cardenas <scardenas@brooklynsteamcenter.org>
2025-01-08 10:53:35 -0500

    cart page set up

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from dynaconf import Dynaconf
 import flask_login
 import pymysql
 
-app = Flask(__name__)
-
 conf = Dynaconf(
     settings_files = ["settings.toml"]
 )
+
+app = Flask(__name__) 
+app.secret_key = conf.secret_key
+
+
 
 login_manger = flask_login.LoginManager()
 login_manger.init_app(app)
@@ -18,6 +21,34 @@ class User:
     is_authenticated = True 
     is_anonymous = False 
     is_active = True
+
+    def __init__(self, user_id, username, email, first_name, last_name):
+        self.id = user_id 
+        self.username = username 
+        self.email = email
+        self.first_name = first_name 
+        self.last_name = last_name
+
+        def get_id(self):
+            return str(self.id)
+        
+
+@login_manger.user_loader
+def load_user(user_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT * FROM `Customer` WHERE `id` = {user_id}")
+
+    result = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+
+    if result is not None:
+        return User(result["id"], result["username"], result["email"], result["first_name"], result["last_name"])
+    
+        
 
 
 
@@ -80,9 +111,6 @@ def product_page(product_id):
 
 
 
-
-
-
     return result
 
 
@@ -91,45 +119,44 @@ def signup():
  if flask_login.current_user.is_authenticated:
         return redirect ("/")
  else:
-
-
     if request.method == "POST":
            
      first_name = request.form["first_name"]
      last_name = request.form["last_name"]
     
+     username = request.form["username"]
+     email = request.form["email"]
 
-     Email_address = request.form["Email_address"]
-     Password = request.form["Password"]
-     
+
+     password = request.form["password"]
+     confirmpassword = request.form["confirmpassword"]
+
+     if len(password) < 12:
+         flash("Your password must be atleast 12 characters")
+         return render_template("sign_up.html.jinja")
      conn = connect_db()
 
      cursor = conn.cursor()
      try:
         cursor.execute(f"""
-                        return redirect("/sgin_in")
-
-
-
-
-                INSTERT INTO 'Customer'
-                    ('first_name', 'last_name', 'username','email','password')
-                    VALUES
-                     ('{first_name}', '{last_name}', '{Email_address}','{Password}')
+                INSERT INTO `Customer`
+                    (`first_name`, `last_name`, `username`, `email`, `password`)
+                VALUES
+                     ('{first_name}', '{last_name}', '{email}','{password}','{username}')
          """)       
-     except pymysql.error.IntegrityError:
+     except pymysql.err.IntegrityError:
         flash("sorry this email has already taken please try another one")
+     else:
+         return redirect("/signin")
+     finally:
+         cursor.close()
+         conn.close()
     
     return render_template("sign_up.html.jinja")
 
 
 
 
-
-
- @app.route('/cart')
- def cart():
-      return "cart page "
 
 
 
@@ -153,29 +180,19 @@ def add_to_cart(product_id):
 
 #bring user to cart page 
 
-@app.route("/cartpage")
-@flask_login.login_required
-def cart_page(): 
-    conn = connect_db()
-    cursor = conn.cursor()
-
-    customer_id = flask_login.current_user.id
-
-    cursor.excute("SELECT  `name` , `price`, `gyt`, `image`, `product_id`, `cart`, `id` * FROM `cart_page` WHERE `customer_id` = {custmomer_id}; ")
-
-    results = cursor.fetchall()
-
-    cursor.close()
-
-    conn.close()
-
-
-
-    return render_template("cart_page.html.jinja", products=results)
 
 
 
 
+@app.route('/cart')
+def cart():
+    return render_template('cart_page.html.jinja')
+    # @app.route('/cart/<cart_id>/del", method=["POST"])
+    #            @flask_login.login_required
+    #            def delete_cart(cart_id):
+               
+    #            conn = connect_db
 
-   
+    #            cursor = conn.cursor()
+
  
